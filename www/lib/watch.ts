8f7eb97999619b713f8c80da9dbe7d9c7c6037d1
Dmitriy Lazarev <w@kich.dev>
2023-12-04 19:25:44 +0400

    Add frontside.com documentation minisite
    Signed-off-by: Dmitriy Lazarev <w@kich.dev>

---
+++ www/lib/watch.ts
@@ -0,0 +1,76 @@
+import {
+  action,
+  filter,
+  first,
+  pipe,
+  resource,
+  run,
+  sleep,
+  type Stream,
+  stream,
+  type Subscription,
+} from "effection";
+
+import { parse } from "https://deno.land/std@0.188.0/flags/mod.ts";
+
+import { useCommand } from "./use-command.ts";
+
+await run(function* () {
+  let scriptargs = parse(Deno.args, {
+    "--": true,
+  });
+
+  let paths = scriptargs["_"].map(String);
+  let [cmd, ...args] = scriptargs["--"];
+
+  let ignores = [".#"].concat(scriptargs.ignore ?? []);
+
+  console.log(`watch: ${JSON.stringify(paths)}`);
+  console.log(`run: ${cmd} ${args.join(" ")}`);
+
+  yield* action<void>(function* (resolve) {
+    Deno.addSignalListener("SIGINT", resolve);
+    try {
+      while (true) {
+        yield* action(function* (restart) {
+          let change = pipe(
+            useFsWatch(paths),
+            filter(function* (event) {
+              return !event.paths.some((path) =>
+                ignores.some((ignore) => path.includes(ignore)),
+              );
+            }),
+          );
+
+          yield* useCommand(cmd, { args });
+
+          yield* first(change);
+
+          yield* sleep(100);
+
+          console.log("changes detected, restarting...");
+
+          restart();
+        });
+      }
+    } finally {
+      Deno.removeSignalListener("SIGINT", resolve);
+    }
+  });
+});
+
+function useFsWatch(paths: string | string[]): Stream<Deno.FsEvent, never> {
+  return {
+    subscribe() {
+      return resource(function* (provide) {
+        let watcher = Deno.watchFs(paths);
+        try {
+          let subscription = yield* stream(watcher).subscribe();
+          yield* provide(subscription as Subscription<Deno.FsEvent, never>);
+        } finally {
+          watcher.close();
+        }
+      });
+    },
+  };
+}
